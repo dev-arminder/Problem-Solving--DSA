25ef67826d912f54698af6dac1523a55d22f9e5c
dev-arminder <mail.to.arminder.singh@gmail.com>
2022-05-26 23:48:23 +0530

    Working on Hash Map

--- JavaScript/HashMap/index.js
+++ JavaScript/HashMap/index.js
@@ -2,14 +2,40 @@ const defaultHashTableSize = 32;
 const LinkedList = require("../LinkedList/singlyLinkedList/LinkedList");
 
 class HashTable {
+  /**
+   * @param {number}
+   */
   constructor(tableSize = defaultHashTableSize) {
     this.tableSize = tableSize;
-    this.bucket = [];
+    this.buckets = Array(tableSize)
+      .fill(null)
+      .map(() => new LinkedList());
+    this.keys = {};
   }
-  /*
-   * Calculate hash value - module by 32
+
+  /**
+   * @use - Ti calculate hash value based upon keys
+   * @param - {any}
+   * @return - {number}
    */
   hash(key) {
-    return key % this.tableSize;
+    const hash = Array.from(key).reduce(
+      (hashAccumulator, keySymbol) => hashAccumulator + keySymbol.charCodeAt(0),
+      0
+    );
+
+    // Reduce hash number so it would fit hash table size.
+    return hash % this.tableSize;
+  }
+
+  /**
+   * @param - {string key} - any
+   * @param - {Any value}
+   */
+  set(key, value) {
+    const keyHash = this.hash(key);
+    this.keys[key] = keyHash;
+    const bucketLinkedList = this.buckets[keyHash];
+    const node = bucketLinkedList.findByValue();
   }
 }
